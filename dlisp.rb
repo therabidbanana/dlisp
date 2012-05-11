ac4b0d063e2357bc26731cb346a2f5d2fd86bad9
David Haslem <therabidbanana@gmail.com>
2012-05-11 10:07:19 -0400

    Comment just a bit

--- dlisp.rb
+++ dlisp.rb
@@ -1,4 +1,6 @@
 class O_o < StandardError; end
+# Break string into tokens - note the ending parens can be sloppy
+# we don't properly count them
 def parse(string)
   tokens = string.is_a?(Array) ? string : string.gsub(/([\(\)])/,' \1 ').split
   list = nil; t = tokens.shift
@@ -8,21 +10,31 @@ def parse(string)
   end
   list
 end
+# Parse nums, otherwise symbol
 def atomize(i)
   ret = Integer(i);rescue;begin;ret ||= Float(i);rescue;ret ||= i.to_sym; end
 end
 def call(x, env=new_env)
+  # Simple returns
   return env[x] if x.is_a?(Symbol)
   return x unless x.is_a?(Array)
+  # Break list out, get first three expressions
   (atom,b,c) = x;
-  case atom when :quote; b; when :define; env[b] = call(c, env); nil  # simple return, simple define - calling before storing
-  when :set!; env[b, call(c,env)]; nil # Set only if b defined, always nil return
-  when :begin;  (a, *d) = x; d.map{|exp| call(exp, env) }.last # (a, *b) = x for (cdr x), then map
+  # quote instantly returns b
+  case atom when :quote; b;
+  # define - calls c before storing as b
+  when :define; env[b] = call(c, env); nil
+  # Set only if b defined, always nil return
+  when :set!; env[b, call(c,env)]; nil
+  # (a, *b) = x for pulling off first atom; then call all expressions; get last
+  when :begin;  (a, *d) = x; d.map{|exp| call(exp, env) }.last
+  # return anonymous function
   when :lambda; (->(*args){call(c, new_env(b, args, env)) })
-  when :if; alt = x.last; call(call(b, env) ? c : alt, env) # define last, eval either c or last depending on eval of b
+  # define last, eval either c or last depending on eval of b
+  when :if; alt = x.last; call(call(b, env) ? c : alt, env)
   else
-    (n, *d) = x.map{|exp| call(exp, env)};
-    n[*d] # Pull out initial procedure name and then call it with list of args evaled
+    # Pull out initial procedure name and then call it with list of args evaled
+    (n, *d) = x.map{|exp| call(exp, env)}; n[*d]
   end
 end
 class Env
@@ -45,12 +57,11 @@ end
 def new_env(*args); (a,b,c)=args; a||=[]; b||=[]; Env.new(a,b,c); end
 def global_env
   glob = {
-    :+ => ->(a,b){a+b},
-    :> => ->(a,b){a>b},
-    :< => ->(a,b){a<b},
-    :* => ->(a,b){a*b}
+    :+ => ->(a,b){a+b}, :* => ->(a,b){a*b}, :-   => ->(a,b){a-b}, :/ => ->(a,b){a/b},
+    :> => ->(a,b){a>b}, :< => ->(a,b){a<b}, :'=' => ->(a,b){a==b},
+    :>= => ->(a,b){a>=b}, :<= => ->(a,b){a<=b},
   }
   Env.new(glob.keys,glob.values,{})
 end
 
-warn call(parse("(begin (define sexy (lambda (r) (* 3.141592653 (* r r))))(if (> 1 0) (sexy 3) (quote (0))))")).inspect
+puts call(parse("(begin (define area_circ (lambda (r) (* 3.141592653 (* r r))))(if (> 1 0) (area_circ 3) (quote (0))))")).inspect
